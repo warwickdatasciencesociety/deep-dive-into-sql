7473df499a7a8bdc50b8ec77998b3d9b26d1e069
Matthew Bardsley <Matthew.Bardsley@warwick.ac.uk>
2021-09-09 15:21:32 +0100

    added local user credentials to init script

--- server/init.sh
+++ server/init.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # More info: https://relational.fit.cvut.cz/about
 # List databases to import (case-senstive)
+echo "CREATE USER 'guest'@'%' IDENTIFIED BY 'relational'" > /docker-entrypoint-initdb.d/1.sql
 db_names=(
     genes
     Carcinogenesis
@@ -19,7 +20,7 @@ for db_name in "${db_names[@]}"; do
     tb_list=`mysql --defaults-extra-file=config.cnf -AN -e"$q"`
     # Create database initialisation script
     params="--column-statistics=0 $db_name $tb_list"
-    echo "CREATE DATABASE IF NOT EXISTS ${db_name,,}; USE ${db_name,,};" > \
+    echo "CREATE DATABASE IF NOT EXISTS ${db_name,,}; USE ${db_name,,}; GRANT SELECT ON ${db_name,,}.* TO 'guest'@'%';" > \
         /docker-entrypoint-initdb.d/${db_name,,}.sql
     mysqldump --defaults-extra-file=config.cnf $params >> \
         /docker-entrypoint-initdb.d/${db_name,,}.sql

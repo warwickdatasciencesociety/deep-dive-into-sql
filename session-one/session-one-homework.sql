d7ec52c3299e3e6b7eb8d6a9278991cda52ff2ab
Patricio Hernandez Senosiain <hdzsen.patricio@gmail.com>
2021-09-16 00:56:10 +0100

    Added session two presentation

--- session-one/session-one-homework.sql
+++ session-one/session-one-homework.sql
@@ -5,8 +5,24 @@ To receive a certificate email all four
 homework files to education@wdss.io
 
 To start out with this homework, connect to the
-course database
+course database with the following details
 
+- Host: sql.wdss.io
+- Port: 33061
+- User: guest
+- Password: relational
+
+The homework exercises below will make use of the 'genes' database.
+With the help of this database and our SQL skills we will retrieve
+data to help us analyse the characteristics, functions, and interactions
+of different genes.
 */
 
--- Question 1
+/* Question 1: Select the 'GeneID' and 'Function' column from the 'Genes' table. Name three
+               different types of gene functions that you can see upon inspection.
+/*
+
+
+/* Question 2: Select the
+-- Question 3
+-- Question 4

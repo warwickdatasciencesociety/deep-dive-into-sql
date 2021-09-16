eaa52659067a0e4ef8fa33e5a4dbaff7ccdcbf31
Patricio Hernandez Senosiain <hdzsen.patricio@gmail.com>
2021-09-16 16:49:15 +0100

    Update session-one-homework.sql

--- session-one/session-one-homework.sql
+++ session-one/session-one-homework.sql
@@ -18,11 +18,34 @@ data to help us analyse the characteristics, functions, and interactions
 of different genes.
 */
 
-/* Question 1: Select the 'GeneID' and 'Function' column from the 'Genes' table. Name three
-               different types of gene functions that you can see upon inspection.
-/*
+-- USING THE GENES TABLE
+
+/* Question 1:
+Select the 'GeneID' and 'Function' columns. Use 'ID' as an
+alias for GeneID. Name three different types of gene functions that
+you can see upon inspection.
+*/
 
 
-/* Question 2: Select the
--- Question 3
--- Question 4
+/* Question 2:
+Select all of the genes located in the nucleus and with
+'METABOLISM' as their function.
+*/
+
+/* Question 3:
+Select the top 20 genes located in the cytoplasm with strictly
+more than 2 chromosones.
+*/
+
+/* Question 4:
+Select all of the 'Essential' genes located in either
+the cytoplasm or mitochondria.
+*/
+
+/* Question 5:
+Select the top 10 genes without exactly 2 chromosones and belonging to the
+'ATPases' gene class.
+*/
+
+
+-- USING THE INTERACTIONS TABLE

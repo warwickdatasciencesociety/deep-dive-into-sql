fb19704ad5db9e2f6f47a1c70fda8b884a14dcd8
Dominika Dodzia Daraż <42532942+Precel2000@users.noreply.github.com>
2021-09-23 16:46:51 +0100

    Create session-three-exercises.sql

---
+++ session-three/session-three-exercises.sql
@@ -0,0 +1,58 @@
+-- genes table
+/* QUESTION 1
+Localisation of genes with type 'Genetic'
+*/
+
+
+/* QUESTION 2
+pairs of gene ID with the same localisation. 
+Take care to list UNIQUE pairs. (a,b) and (b,a) are the same pair.
+*/
+
+
+/* QUESTION 3 and 4
+Return the ID of any genes G235204 has a "Physical" interaction with.
+How many does G234065 have any type of interaction with?
+*/
+
+
+-- walmart table
+/* QUESTION 5
+Select the first 10 rows from the key table, ordered by station_nbr descending.
+Can you see how this query could be interpreted in 2 different ways? 
+Carry out both possible interpretations
+*/
+
+
+/* QUESTION 6
+What are the station number that experienced the highest recorded snowfall? Use a subquery
+*/
+
+
+/* QUESTION 7
+Using your answer to the previous question, indicate which store corresponds to the highest snowfall. 
+Use a NATURAL JOIN
+*/
+
+
+/* QUESTION 8
+At what time did the sun rise on 2012-02-05 at the stores that sold 
+more than 120 000 of units in total of all items?
+*/
+
+
+-- salesdb table
+/* QUESTION 9
+Recall that Full Outer Join is no longer supported by MySQL
+How could you perform an equivalent operation? 
+Read up on UNION and UNION ALL.
+Perform a full outer join on the tables Employees and Customers. We want the data to display in a single row if a customer and an employee have the same ID
+Do allow duplicate rows.
+*/
+
+
+/* QUESTION 10
+Carry out the query from Question 9, this time not allowing duplicate rows.
+Are the results any different. Is either query faster?
+*/
+

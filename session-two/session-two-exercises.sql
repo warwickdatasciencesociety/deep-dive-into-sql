dd25e369e182beb9baafeafc35992dcb74a6db44
Dominika Dodzia Daraż <42532942+Precel2000@users.noreply.github.com>
2021-09-24 15:52:07 +0100

    Create session-two-exercises.sql

---
+++ session-two/session-two-exercises.sql
@@ -0,0 +1,55 @@
+-- genes
+/* QUESTION 1
+What are the types of interactions possible? How many different interactions are there?
+*/
+
+
+/* QUESTION 2
+What is the maximum and minimum expression_correlation between genes? 
+*/
+
+/* QUESTION 3
+Right-click on the Genes Table, then click on "View Structure". 
+Which columns are allowed to contain a NULL value? 
+Do any of them contain a NULL value?
+*/
+
+
+-- restbase
+
+/* QUESTION 4
+Select the ids and location(city) of all restaurants whose address contains the word "san"
+*/
+
+/* QUESTION 5
+which cities hava a name composed of at least 2 words? How many of exactly 2 words?
+*/
+
+/* QUESTION 6
+Using you queries from Question 5 and COUNT() how many cities are there, whose 
+names are longer than 2 words? You may use more than 1 query to answer this
+*/
+
+
+-- walmart
+/* QUESTION 7
+Select all rows from the weather table, for which the time of snowfall has not been recorded but
+total precipitation has
+*/
+
+
+/* QUESTION 8
+Select all station numbers for which the average maximum temperature was above 60
+*/
+
+
+/* QUESTION 9
+Select all station numbers and their average snowfall. Order by maximum average temperature (lowest to highest). 
+Rename the appropriate column to "mean_snowfall"
+*/
+
+/* QUESTION 10
+Modify your previous query to order by mean snowfall from lowest to highest and by 
+maximum average temperature from highest to lowest
+*/
+
